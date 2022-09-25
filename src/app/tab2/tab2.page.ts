3981e5e723e04c5e839d99152b416329f83f18d0
LucasVieiraS <lucas.silva3759@etec.sp.gov.br>
2022-09-25 15:09:14 -0300

    feat: metronome

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -7,6 +7,12 @@ import { Component } from '@angular/core';
 })
 export class Tab2Page {
 
+  isRecording = false;
+
   constructor() {}
+
+  setRecording() {
+    this.isRecording = !this.isRecording;
+  }
 
 }
--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -7,6 +7,52 @@ import { Component } from '@angular/core';
 })
 export class Tab3Page {
 
-  constructor() {}
+  bpm = 145;
+  holdingBPM = false;
+
+  bpm2 = 4;
+
+  constructor() { }
+
+  /* BEATS PER MINUTE */
+  setBPM(increment: number) {
+    if (this.bpm + increment > 1 && this.bpm + increment < 250) {
+      this.bpm += increment;
+    } else {
+      this.bpm = 145;
+    }
+  }
+
+  startLoop(increment: number) {
+    setTimeout(() => {
+      if (this.holdingBPM === true) {
+        if (this.bpm + increment > 1 && this.bpm + increment < 250) {
+          this.bpm += increment;
+        } else {
+          this.bpm = this.bpm >= 249 && 1 || 250 ;
+        }
+        this.startLoop(increment);
+      }
+    }, 1500);
+  }
+
+  setHoldingBPM(increment: number) {
+    this.holdingBPM = true;
+    this.startLoop(increment);
+  }
+
+  stopHoldingBPM() {
+    this.holdingBPM = false;
+  }
+
+  /* BEATS PER MEASURE */
+  setBPM2(increment: number) {
+    this.holdingBPM = false;
+    if (this.bpm2 + increment > 0 && this.bpm2 + increment < 15) {
+      this.bpm2 += increment;
+    } else {
+      this.bpm2 = this.bpm2 === 15 && 1 || 15 ;
+    }
+  }
 
 }

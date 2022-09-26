ea641f79652563d3ed69fa61a21a4ecc9f79a34b
LucasVieiraS <lucas.silva3759@etec.sp.gov.br>
2022-09-25 22:23:49 -0300

    feat: guitar tuner

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+import { Component } from '@angular/core';
 
 declare const p5;
 declare const ml5;
@@ -8,71 +10,73 @@ declare const ml5;
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page implements OnInit {
-  modelUrl = 'https://cdn.jsdelivr.net/gh/ml5js/ml5-data-and-models/models/pitch-detection/crepe/';
+export class Tab1Page {
 
   guitarIcon = '../../assets/icon/guitar.svg';
   bassIcon = '../../assets/icon/bass.svg';
 
-  currentFrequency = 0;
   noteName = 'C#';
+  pitch;
+
+  onCanvasHidden = false;
+  frequency = 0;
 
   p5;
-  onCanvasHidden = false;
-  pitch;
-  freq = 0;
-
-  audioContext = new AudioContext();
-  mic = new p5.AudioIn();
-
-  notes = [
-    { note: 'E', freq: 82.41 },
-    { note: 'A', freq: 110.00 },
-    { note: 'D', freq: 146.83 },
-    { note: 'G', freq: 196.00 },
-    { note: 'B', freq: 246.94 },
-    { note: 'E', freq: 329.63 }
-  ];
-
   constructor() {}
+
 
   createCanvas() {
     this.onCanvasHidden = true;
     new p5((p) => this.sketch(p));
   }
-
-  gotPitch(error: string, frequency: number) {
-    if (error) {
-      console.error(error);
-    }
-    if (frequency) {
-        this.freq = frequency;
-    }
-    this.pitch.getPitch( this.gotPitch);
-  }
-
-  modelLoaded() {
-    this.pitch.getPitch(this.gotPitch);
-  }
-  loadModel() {
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-    this.pitch = ml5.pitchDetection(this.modelUrl, this.audioContext, this.mic.stream, this.modelLoaded);
-  }
-
-  public sketch(p) {
-
+  private sketch(p) {
+    let freq = 0;
+    const notes = [
+      { note: 'E¹', freq: 82.41 },
+      { note: 'A', freq: 110.00 },
+      { note: 'D', freq: 146.83 },
+      { note: 'G', freq: 196.00 },
+      { note: 'F', freq: 222.22 },
+      { note: 'B', freq: 246.94 },
+      { note: 'E²', freq: 329.63 }
+    ];
     p.setup = () => {
       p.createCanvas(350, 350);
+      const modelUrl = 'https://cdn.jsdelivr.net/gh/ml5js/ml5-data-and-models/models/pitch-detection/crepe/';
+      const audioContext = new AudioContext();
       // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-      this.mic.start(this.loadModel);
+      const mic = new p5.AudioIn();
+      let pitch;
+      mic.start(loadModel);
 
+      // eslint-disable-next-line prefer-arrow/prefer-arrow-functions
+      function loadModel() {
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+        pitch = ml5.pitchDetection(modelUrl, audioContext, mic.stream, modelLoaded);
+      }
+
+      // eslint-disable-next-line prefer-arrow/prefer-arrow-functions
+      function modelLoaded() {
+        pitch.getPitch(gotPitch);
+      }
+      // eslint-disable-next-line prefer-arrow/prefer-arrow-functions
+      function gotPitch(error: string, frequency: number) {
+        if (error) {
+          console.error(error);
+        }
+        if (frequency) {
+            freq = frequency;
+        }
+        pitch.getPitch(gotPitch);
+      }
     };
 
     p.draw = () => {
       let noteDetected;
       let toneDiff = Infinity;
-      this.notes.forEach(note => {
-        const diff = this.freq - note.freq;
+      notes.forEach(note => {
+        const diff = freq - note.freq;
+        this.frequency = Math.floor(freq);
         if (p.abs(diff) < p.abs(toneDiff)) {
           noteDetected = note;
           toneDiff = diff;
@@ -101,12 +105,7 @@ export class Tab1Page implements OnInit {
       p.fill(56, 56, 56);
       p.textAlign(p.CENTER);
       p.textSize(38);
-      p.text(noteDetected.note, p.width / 2, p.height / 2 - 68);
+      this.noteName = noteDetected.note;
     };
   }
-
-  ngOnInit() {
-    this.createCanvas();
-  }
-
 }
